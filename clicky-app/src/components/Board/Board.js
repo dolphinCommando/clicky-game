761580d305ce2c800ec751380802f331cfca8acb
Matthew Buchanan <matt@Matthews-MacBook-Air.local>
2018-07-05 12:09:06 -0400

    Board render, readdir images, randomize array algorithm; Square clean-up

--- clicky-app/src/components/Board/Board.js
+++ clicky-app/src/components/Board/Board.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import './Board.css';
+const fs = require('fs');
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.images = getImageFiles();
+    this.state = {
+      complete: false
+    }
+  }
+  getImageFiles = () => {
+    fs.readdir('~/clicky-app/public/images', (err, files) => {
+      if (err) throw err;
+      console.log(files);
+      return files;
+    });
+  }
+  randomizeImages = () => {
+    let idxRandom = [];
+    let idxTemp = [];
+    let img = this.images;
+    let count = 0;
+    for (let i = 0; i < img.length; i++) {
+      idxTemp.push(i);
+    }
+    while (idxRandom.length < img.length) {
+      if (count > img.length) {
+        console.log('Error: randomizeImages() failed: out of bounds');
+        break;
+      }
+      let randomIdx = Math.floor(Math.random() * idxTemp.length);
+      idxRandom.push(idxTemp[randomIdx]);
+      idxTemp.splice(randomIdx, 1);
+      count++;
+    }
+    console.log(idxRandom);
+    return idxRandom.map(index => img[index]);  
+  }
   render() {
-    
+    const randomImages = randomizeImages();
+    const rows = randomImages.map(image => <Square img={image} />);
+    return (
+      <div className="row">
+        {rows}
+      </div>
+    );
   }
 }
+
+export default Board;
--- clicky-app/src/components/Square/Square.js
+++ clicky-app/src/components/Square/Square.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Square.css';
 
 class Square extends React.Component {
-  state = {
-    click: 0
-  }
-
-  handleClick = () => {
-    this.setState({click: this.state.click + 1});
+  constructor(props) {
+    super(props);
+    this.state = {
+      click: 0
+    }
   }
 
   render() {
+    let img = this.props.img;
     return (
-      <div className="square col-4" onClick={this.handleClick}>
-        <img src={this.props.imageSrc} alt={this.props.imageAlt}></img>
+      <div className="col-4 square" onClick={() => this.setState({click: this.state.click + 1})}>
+        <img src={img} alt={img.substr(0, img.lastIndexOf('.'))}></img>
       </div>
     );
   }
