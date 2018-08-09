1ec7aabdf3f343dbd179fd412cd79bfda092c17d
Matthew Buchanan <mwbuchan92@gmail.com>
2018-08-09 10:04:56 -0400

    added font animation to help users see current score

--- clicky-app/src/components/Score/Score.js
+++ clicky-app/src/components/Score/Score.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Score.css';
 
 const Score = (props) => {
+  let spanClass = "text-left ml-5";
+  if (+props.score > 0) {
+    spanClass += " flash";
+  }
   return (
     <nav className="navbar sticky-top">
-      <span className="text-left ml-5">Characters: {props.score} / {props.total}</span>
+      <span className={spanClass}>Characters: {props.score} / {props.total}</span>
       <span className="text-right mr-5">Games Completed: {props.wins}</span>
     </nav>
   );
